fac5e9a97c99e49c53063cdd19393b07d80e4c21
ThaiNX1 <nxthai22@gmail.com>
2026-04-02 16:41:53 +0700

    Update webpack.config.js

--- apps/customer-service/webpack.config.js
+++ apps/customer-service/webpack.config.js
@@ -1,5 +1,11 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 
-module.exports = composePlugins(withNx(), (config) => {
+module.exports = composePlugins(withNx(), (config, { configuration }) => {
+  // Only minify for production builds
+  // withNx() sets mode='none', so we check the Nx configuration instead
+  if (configuration === 'production') {
+    config.optimization = config.optimization || {};
+    config.optimization.minimize = true;
+  }
   return config;
 });
